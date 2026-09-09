0573905538683340f3dbc118a9d54a18e20f558e
racistcode <akshatparab55@gmail.com>
2026-09-09 21:23:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,3 +4,21 @@ import sqlite3
 def connect_db():
     connection = sqlite3.connect("expenses.db")
     return connection
+
+
+def create_table():
+    connection = connect_db()
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount REAL NOT NULL,
+            category TEXT NOT NULL,
+            description TEXT
+        )
+    """)
+
+    connection.commit()
+    connection.close()

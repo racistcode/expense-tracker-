877b580728c5e3f77c7678153133941010ef6ead
racistcode <akshatparab55@gmail.com>
2026-09-09 22:25:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from database import create_database
 
 app = Flask(__name__)
+
+# Create the database and expenses table
+create_database()
 
 
 @app.route("/")

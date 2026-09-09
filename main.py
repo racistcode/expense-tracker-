4901fc35f868cd19a026d9adf5ebaba86c8e4e9e
racistcode <akshatparab55@gmail.com>
2026-09-09 22:13:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return """
-    <h1>Expense Tracker</h1>
-    <p>Flask is working!</p>
-    """
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
